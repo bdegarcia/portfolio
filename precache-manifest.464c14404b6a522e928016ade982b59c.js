60404c0ed0ad7e1973270ad0f386c3af60621686
Ben de Garcia <degarcia.ben@gmail.com>
2020-09-29 22:29:07 -0500

    Updates

---
+++ precache-manifest.464c14404b6a522e928016ade982b59c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52b1471623ca89c3e99af5ff96cd491a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "88438465f3963be787e4",
+    "url": "/portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "eb0b53000eef808b8967",
+    "url": "/portfolio/static/css/main.8b0df5b9.chunk.css"
+  },
+  {
+    "revision": "88438465f3963be787e4",
+    "url": "/portfolio/static/js/2.81742f4e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.81742f4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb0b53000eef808b8967",
+    "url": "/portfolio/static/js/main.262d6f0b.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "e174cdf04e7b2b4754b5cff218d8996a",
+    "url": "/portfolio/static/media/ben.e174cdf0.jpg"
+  },
+  {
+    "revision": "4e16ba685f35c32c5fd42f9a8ddb5f38",
+    "url": "/portfolio/static/media/resume.4e16ba68.pdf"
+  }
+]);
